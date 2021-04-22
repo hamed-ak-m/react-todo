8bed0d8dab1cf83bc1dc39c62a5ce83f4c544218
hamed-akbarzadeh-m <akbarzadeh.m.hamed@protonmail.com>
2021-04-22 15:43:37 +0430

    step 1 + style added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+/* eslint-disable react/style-prop-object */
+/* eslint-disable no-unreachable */
 import React from "react";
-
+import "./style.css";
 const App = () => {
-  return <div>salam</div>;
+  return (
+    <div className={"container"}>
+      <input />
+      <button>add</button>
+      <div className={"list"}>
+        <div className={"task"}>
+          <p className={"taskTitle"}>do a work</p>
+        </div>
+        <div className={"task"}>
+          <p className={"taskTitle"}>do a task</p>
+        </div>
+        <div className={"task"}>
+          <p className={"taskTitle"}>do a work</p>
+        </div>
+        <div className={"task"}>
+          <p className={"taskTitle"}>do a task</p>
+        </div>
+        <div className={"task"}>
+          <p className={"taskTitle"}>do a work</p>
+        </div>
+        <div className={"task"}>
+          <p className={"taskTitle"}>do a task</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
